a4a289ec6cbac8dc9f16bf41f5d0b5c69f1cbd44
rpounceyjr <rpounceyjr@gmail.com>
2020-05-05 15:12:17 -0400

    get method working in controller, rendering properly to index.handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,6 +3,18 @@ const burger = require("../models/burger");
 
 const router = express();
 
+//this is the backend to get all burger names and
+//render them on the index.handlebars
+router.get("/", (req, res) => {
+    burger.selectAll((data) => { 
+       const burgersData = {
+           burgers: data
+       };
 
+        console.log(burgersData);
+        res.render("index", burgersData)
+    })
+    //now need to res.render this list to the template
+})
 
 module.exports = router;
