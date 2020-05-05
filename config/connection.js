4fbd258627bac33f6feeb88d1ac6d753acf80ddf
rpounceyjr <rpounceyjr@gmail.com>
2020-05-04 21:33:25 -0400

    created and seeded db. set up conection.connect in connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,4 +9,9 @@ const connection = mysql.createConnection({
     database: "burgers_db"
 });
 
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
+
 module.exports = connection;
