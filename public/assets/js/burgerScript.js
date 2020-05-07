dcce8344013715ddae9887799c4cda36cc8cace6
rpounceyjr <rpounceyjr@gmail.com>
2020-05-07 10:07:28 -0400

    moved html routes from main.handlebars to burgerScript.js and linked that to main,handlebars.

--- public/assets/js/burgerScript.js
+++ public/assets/js/burgerScript.js
@@ -1,4 +1,3 @@
-//===========ajax functions============
 //add a new burger
 $(".create-form").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
@@ -8,6 +7,7 @@ $(".create-form").on("submit", function (event) {
         name: $("#burger-add").val().trim(),
     };
     console.log("clicked")
+    console.log(newBurger);
 
 
     // Send the POST request.
@@ -21,4 +21,28 @@ $(".create-form").on("submit", function (event) {
     })
 })
 
-//======================================
+//update a burger 
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-devoured").on("click", function(event) {
+    const id = $(this).data("id");
+    const devouredUpdate = $(this).data("devoured");
+    console.log($(this).data("devoured"));
+
+    const devouredState = {
+      devoured: devouredUpdate
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devouredState
+    }).then(
+      function() {
+        console.log("changed devoured to", devouredUpdate);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+})
