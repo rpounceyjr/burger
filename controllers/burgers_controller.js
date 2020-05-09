cb147290bdb0a316f2b328d213951b91fff87f04
rpounceyjr <rpounceyjr@gmail.com>
2020-05-09 15:15:28 -0400

    added back in the readme and a few comments.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const burger = require("../models/burger");
-
 const router = express();
 
 //this is the backend to get all burger names and
@@ -15,21 +14,22 @@ router.get("/", (req, res) => {
         res.render("index", burgersData)
     })
 })
-
+//this route adds a burger burger name to the table
 router.post("/api/burgers", (req, res) => {
     console.log(req.body.name)
     burger.insertOne(req.body.name, (result) => {
         console.log(result);
+        //reloads the page to get the new burger 
         res.redirect("/")
     });
 });
 
-
+//this is the route to change a burger's "devoured" state
 router.put("/api/burgers/:id", (req, res) => {
     const condition = "id = " + req.params.id;
   
     console.log("condition", condition);
-  
+  //updates the burger's devoured state to true
     burger.updateOne(1, condition, function(result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
